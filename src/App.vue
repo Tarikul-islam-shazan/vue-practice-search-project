eedcb0c06a02f177357d63a7ef217dfe87bebbdc
Tarikul-islam-shazan <Tarikul@brainstation23.com>
2022-01-25 19:44:34 +0600

    search project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    width: 5em;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script>
+import { ref } from 'vue'
+import SearchResults from './SearchResults.vue';
+
+export default{
+  components: {
+    SearchResults,
+  },
+  /**
+  * @author tarikul
+  * @param void
+  * @return {String}
+  */
+  setup() {
+    const query = ref("");
+    const reset = (evt) => {
+      query.value = "";
+    }
+    return {
+      query,
+      reset,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h2>Search Product</h2>
+    <input type="text" placeholder="serch here" v-model='query'/>
+    <br/>
+    <button @click="reset">Reset</button>
+    <search-results :query='query'/>
+  </div>
+</template>
+
+
+<style>
+  input[type='text'] {
+    margin-bottom: 20px;
+    padding: 10px;
+  }
+</style>
